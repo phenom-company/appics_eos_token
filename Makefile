656b4260adf70ee2999852a6aea24173d7005d20
avral <qweasd_96@mail.ru>
2019-07-05 22:45:04 +0300

    initial

---
+++ Makefile
@@ -0,0 +1,19 @@
+wallet_pasword=
+cleos_path=cleos
+source_path=$(PWD)
+contract=apx
+
+unlock_wallet:
+	$(cleos_path) wallet unlock --password $(wallet_pasword)
+
+import_keys:
+	$(cleos_path) wallet import --private-key 5Jg7y...
+
+build:
+	eosio-cpp -w -I ./include --abigen $(source_path)/src/eosio.token.cpp -o $(source_path)/eosio.token.wasm
+
+#buy_ram: cleos_path  system buyram $(contract) $(contract) --kbytes 500
+
+deploy:
+	$(cleos_path) set contract $(contract) $(source_path) eosio.token.wasm eosio.token.abi -p $(contract)@active
+	$(cleos_path) set account permission $(contract) active --add-code
---
+++ include/eosio.token/eosio.token.hpp
@@ -0,0 +1,91 @@
+#pragma once
+
+#include <eosiolib/asset.hpp>
+#include <eosiolib/eosio.hpp>
+
+#include <string>
+
+#define APX symbol_code("APX")
+
+namespace eosiosystem {
+   class system_contract;
+}
+
+namespace eosio {
+
+   using std::string;
+
+   class [[eosio::contract("eosio.token")]] token : public contract {
+      public:
+         using contract::contract;
+
+         [[eosio::action]]
+         void create( name   issuer,
+                      asset  maximum_supply);
+
+         [[eosio::action]]
+         void mint( name to, asset quantity, string steem_tx, string memo );
+
+         [[eosio::action]]
+         void issue( name to, asset quantity, string memo );
+
+         [[eosio::action]]
+         void retire( asset quantity, string memo );
+
+         [[eosio::action]]
+         void transfer( name    from,
+                        name    to,
+                        asset   quantity,
+                        string  memo );
+
+         [[eosio::action]]
+         void open( name owner, const symbol& symbol, name ram_payer );
+
+         [[eosio::action]]
+         void close( name owner, const symbol& symbol );
+
+         static asset get_supply( name token_contract_account, symbol_code sym_code )
+         {
+            stats statstable( token_contract_account, sym_code.raw() );
+            const auto& st = statstable.get( sym_code.raw() );
+            return st.supply;
+         }
+
+         static asset get_balance( name token_contract_account, name owner, symbol_code sym_code )
+         {
+            accounts accountstable( token_contract_account, owner.value );
+            const auto& ac = accountstable.get( sym_code.raw() );
+            return ac.balance;
+         }
+
+         using create_action = eosio::action_wrapper<"create"_n, &token::create>;
+         using mint_action = eosio::action_wrapper<"mint"_n, &token::mint>;
+         using issue_action = eosio::action_wrapper<"issue"_n, &token::issue>;
+         using retire_action = eosio::action_wrapper<"retire"_n, &token::retire>;
+         using transfer_action = eosio::action_wrapper<"transfer"_n, &token::transfer>;
+         using open_action = eosio::action_wrapper<"open"_n, &token::open>;
+         using close_action = eosio::action_wrapper<"close"_n, &token::close>;
+      private:
+         struct [[eosio::table]] account {
+            asset    balance;
+
+            uint64_t primary_key()const { return balance.symbol.code().raw(); }
+         };
+
+         struct [[eosio::table]] currency_stats {
+            asset    supply;
+            asset    max_supply;
+            name     issuer;
+
+            uint64_t primary_key()const { return supply.symbol.code().raw(); }
+         };
+
+         typedef eosio::multi_index< "accounts"_n, account > accounts;
+         typedef eosio::multi_index< "stat"_n, currency_stats > stats;
+
+         void sub_balance( name owner, asset value );
+         void add_balance( name owner, asset value, name ram_payer );
+         uint64_t get_steem_inflation();
+   };
+
+} /// namespace eosio
